f11c27427921a10421f65c840268e00d6a404f83
PrashantChaudhary-rgb <prashantjaat652@gmail.com>
2021-08-14 17:36:22 +0530

    initia

---
+++ index.js
@@ -0,0 +1,55 @@
+console.log('this is working');
+let browse_btn=document.getElementById("input-btn");
+let input_file=document.getElementById("input-file");
+let drop_zone=document.querySelector('.drop-zone');
+const APP_BASE_URL= "https://file-sharing-myfirst.herokuapp.com/";
+const uploadURL = `${APP_BASE_URL}api/files`;
+const paperurl=`${APP_BASE_URL}sendall`;
+
+drop_zone.addEventListener('dragover',(e)=>
+{
+    e.preventDefault();
+    if(!drop_zone.classList.contains('dragged'))
+    drop_zone.classList.add('dragged');
+    
+});
+drop_zone.addEventListener('dragleave',()=>
+{
+    drop_zone.classList.remove('dragged');
+});
+drop_zone.addEventListener('drop',(e)=>{
+    e.preventDefault();
+    drop_zone.classList.remove('dragged');
+    console.log(e);
+    const file=e.dataTransfer.files;
+    console.log(file);
+   
+    if(file.length)
+    {
+        console.log('file store wale function ko call karenge ab');
+        input_file.files=file;
+        console.log(input_file.value);
+        uploadfile();
+    }
+    
+});
+
+
+browse_btn.addEventListener('click',()=>
+{
+    input_file.click();
+})
+
+const uploadfile=()=>
+{
+var data=new FormData();
+data.append('myfile',input_file.files[0])
+
+fetch(uploadURL,
+{
+    method:'POST',
+    body:data
+}).then(
+    response => response.json() 
+  ).then(function(data){ console.log(  data.file )  })
+}
